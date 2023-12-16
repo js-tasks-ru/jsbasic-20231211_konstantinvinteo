e0faf256000b15eeda820c5174306218e50b4d63
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-12-16 18:41:53 +0300

    chore: need finish

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (salaries.hasOwnProperty(Number)) {
+      sum += salaries[key];
+      return sum;
+    }
+  }
 }
