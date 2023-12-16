c33c0cb5469562f785b618ae46327abd8d76bca1
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-12-16 13:44:40 +0300

    refactor: truncate

--- 1-module/5-task/task.test.js
+++ 1-module/5-task/task.test.js
@@ -2,7 +2,7 @@ describe('1-module-5-task', () => {
   it('должна усекать строку ', () => {
     let baseStr = 'Вот, что мне хотелось бы сказать на эту тему:';
     let resultStr = 'Вот, что мне хотело…';
-
+ 
     expect(truncate(baseStr, 20)).toBe(resultStr);
   });
 
