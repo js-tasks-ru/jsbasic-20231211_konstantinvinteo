9262a0fa77e1c294c62c1fec228c76b2a8c0a640
KonstantinSeiler <ssteammoi@mail.ru>
2024-01-07 20:19:48 +0300

    feat: add class for task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
+  }
+
+  createTable() {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    this.rows.forEach((row) => {
+      const tr = document.createElement("tr");
+
+      for (const key in row) {
+        const td = document.createElement("td");
+        td.innerText = row[key];
+        tr.appendChild(td);
+      }
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "X";
+      deleteButton.addEventListener("click", () => {
+        tbody.removeChild(tr);
+      });
+
+      tr.appendChild(deleteButton);
+      tbody.appendChild(tr);
+    });
+
+    table.appendChild(tbody);
+    return table;
   }
 }
