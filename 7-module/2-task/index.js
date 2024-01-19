dfbabe6f9c12c86b98ea9c1c320f3e229938e894
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2024-01-19 12:24:02 +0300

    fix: some class modal feat: 8.1 - 9.1 task

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,62 +1,66 @@
 import createElement from "../../assets/lib/create-element.js";
+
 export default class Modal {
   constructor() {
-    this.modal = this.renderModal();
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.render();
+    this.mouseClose();
   }
 
-  renderModal() {
-    const modal = createElement(`
-      <div class="modal">
-        <div class="modal__overlay"></div>
-        <div class="modal__inner">
-          <div class="modal__header">
-            <button type="button" class="modal__close">
-              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon">
-            </button>
-            <h3 class="modal__title"></h3>
-          </div>
-          <div class="modal__body"></div>
+  render() {
+    this.elem = createElement(`
+    <div class="modal">
+      <div class="modal__overlay"></div>
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+          <h3 class="modal__title"></h3>
         </div>
+        <div class="modal__body"></div>
       </div>
+    </div>
     `);
 
-    document.body.append(modal);
-    document.body.classList.add("is-modal-open");
+    this.modalTitle = this.elem.querySelector(".modal__title");
+    this.modalBody = this.elem.querySelector(".modal__body");
 
-    const closeButton = modal.querySelector(".modal__close");
-    closeButton.addEventListener("click", () => this.close());
-
-    document.addEventListener("keydown", this.handleKeyDown);
-
-    return modal;
+    document.addEventListener("keydown", this.keyDownClose);
   }
 
-  setTitle(title) {
-    this.modal.querySelector(".modal__title").textContent = title;
-  }
+  setTitle = (title) => {
+    this.modalTitle.textContent = title;
+  };
 
-  setBody(node) {
-    const modalBody = this.modal.querySelector(".modal__body");
-    modalBody.innerHTML = "";
-    modalBody.append(node);
-  }
+  setBody = (node) => {
+    this.modalBody.innerHTML = "";
+    this.modalBody.append(node);
+  };
 
   open() {
-    this.modal.classList.add("open");
+    this.documentBody = document.querySelector("body");
+    this.documentBody.classList.add("is-modal-open");
+    this.documentBody.append(this.elem);
   }
 
-  close() {
-    this.modal.remove();
-    document.body.classList.remove("is-modal-open");
-    document.removeEventListener("keydown", this.handleKeyDown);
-  }
+  mouseClose = () => {
+    this.elem.onclick = ({ target }) => {
+      let closeBtn = target.closest(".modal__close");
+      if (closeBtn) {
+        this.close();
+      }
+    };
+  };
 
-  handleKeyDown = (event) => {
-    console.log(event);
+  keyDownClose = (event) => {
     if (event.code === "Escape") {
-      console.log(event.code === "Escape");
       this.close();
     }
   };
+
+  close() {
+    document.querySelector("body").classList.remove("is-modal-open");
+    this.elem.remove();
+    document.removeEventListener("keydown", this.keyDownClose);
+  }
 }
--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
@@ -13,32 +13,70 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+    if (document.documentElement.clientWidth <= 767) {
+      this.resetPosition();
+      return;
+    }
+  
+    if (!this.elem.offsetHeight && !this.elem.offsetWidth) {
+      return;
+    }
+  
+    if (window.pageYOffset > this.elem.getBoundingClientRect().top) {
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      );
+  
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1000,
+        right: '10px',
+        left: `${leftIndent}px`
+      });
+    } else {
+      this.resetPosition();
+    }
+  }
+  
+  resetPosition() {
+    Object.assign(this.elem.style, {
+      position: '',
+      top: '',
+      left: '',
+      zIndex: ''
+    });
   }
 }
